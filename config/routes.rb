1e9093e5efc959bdfae2ba851d73cf0567058080
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-07-25 17:12:48 -0400

    reseeded

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   resources :users
   # resources :sessions
 
-  # post '/users', to: 'users#create'
-  get '/users/stay_logged_in', to: 'users#stay_logged_in'
-  post '/users/login', to: 'users#login'
-  get '/users/:id/favorites', to: 'users#favorites'
-
+   # post '/users', to: 'users#create'
+   get '/users/stay_logged_in', to: 'users#stay_logged_in'
+   post '/users/login', to: 'users#login'
+   get '/users/:id/favorites', to: 'users#favorites'
+  
   # post '/products', to: 'snacks#create'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
